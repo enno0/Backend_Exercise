226c8f3df84b0b394d0377d8f3d8c937d0a2965a
enno <kamalaldeenmohamed@gmail.com>
2024-07-22 13:18:27 +0300

    Starting

---
+++ taskmanagementsystem/src/test/java/com/backend_xeercise/taskmanagementsystem/TaskmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_xeercise.taskmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
