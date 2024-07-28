e25c8b7d331f6d0852242907a0059cf039708c34
enno <kamalaldeenmohamed@gmail.com>
2024-07-28 20:15:40 +0300

    fix:email controller

--- taskmanagementsystem/src/main/java/com/backend_xeercise/taskmanagementsystem/restcontroller/EmailController.java
+++ taskmanagementsystem/src/main/java/com/backend_xeercise/taskmanagementsystem/restcontroller/EmailController.java
@@ -1,21 +1,22 @@
-package com.backend_xeercise.taskmanagementsystem.restcontroller;
+// package com.backend_xeercise.taskmanagementsystem.restcontroller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.backend_xeercise.taskmanagementsystem.service.EmailService;
+// import com.backend_xeercise.taskmanagementsystem.service.EmailService;
 
-@RestController
-public class EmailController {
+// @RestController
+// public class EmailController {
 
-    @Autowired
-    private EmailService emailService;
+// @Autowired
+// private EmailService emailService;
 
-    @GetMapping("/send-email")
-    public String sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String text) {
-        emailService.sendSimpleEmail(to, subject, text);
-        return "Email sent successfully";
-    }
-}
+// @GetMapping("/send-email")
+// public String sendEmail(@RequestParam String to, @RequestParam String
+// subject, @RequestParam String text) {
+// emailService.sendSimpleEmail(to, subject, text);
+// return "Email sent successfully";
+// }
+// }
--- taskmanagementsystem/src/main/java/com/backend_xeercise/taskmanagementsystem/service/EmailService.java
+++ taskmanagementsystem/src/main/java/com/backend_xeercise/taskmanagementsystem/service/EmailService.java
@@ -1,21 +1,21 @@
-package com.backend_xeercise.taskmanagementsystem.service;
+// package com.backend_xeercise.taskmanagementsystem.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.mail.SimpleMailMessage;
+// import org.springframework.mail.javamail.JavaMailSender;
+// import org.springframework.stereotype.Service;
 
-@Service
-public class EmailService {
+// @Service
+// public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+// @Autowired
+// private JavaMailSender mailSender;
 
-    public void sendSimpleEmail(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        mailSender.send(message);
-    }
-}
+// public void sendSimpleEmail(String to, String subject, String text) {
+// SimpleMailMessage message = new SimpleMailMessage();
+// message.setTo(to);
+// message.setSubject(subject);
+// message.setText(text);
+// mailSender.send(message);
+// }
+// }
