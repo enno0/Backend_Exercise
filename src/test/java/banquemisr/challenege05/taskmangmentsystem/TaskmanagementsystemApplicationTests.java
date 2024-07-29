d96dff222400c4669b7ef16eac577b215048042a
enno <kamalaldeenmohamed@gmail.com>
2024-07-29 17:55:35 +0300

    test_fix

--- src/test/java/banquemisr/challenege05/taskmangmentsystem/TaskmanagementsystemApplicationTests.java
+++ src/test/java/banquemisr/challenege05/taskmangmentsystem/TaskmanagementsystemApplicationTests.java
@@ -1,14 +1,21 @@
 package banquemisr.challenege05.taskmangmentsystem;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 
 @SpringBootTest
-class TaskmanagementsystemApplicationTests {
+public class TaskmanagementsystemApplicationTests {
+
+	@Autowired
+	private Environment environment;
 
 	@Test
-	void contextLoads() {
-		// left empty
+	public void contextLoads() {
+		System.out.println("Active profiles: " + Arrays.toString(environment.getActiveProfiles()));
+		// Add more logging as needed
 	}
-
 }
