fef3fa738a660046a2b9e1b2a0e686e88cf8a911
enno <kamalaldeenmohamed@gmail.com>
2024-07-30 08:20:20 +0300

    test_class

--- src/test/java/banquemisr/challenege05/taskmangmentsystem/TaskmanagementsystemApplicationTests.java
+++ src/test/java/banquemisr/challenege05/taskmangmentsystem/TaskmanagementsystemApplicationTests.java
@@ -1,13 +1,15 @@
 package banquemisr.challenege05.taskmangmentsystem;
 
-import java.util.Arrays;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Arrays;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class TaskmanagementsystemApplicationTests {
 
 	@Autowired
@@ -16,6 +18,8 @@ public class TaskmanagementsystemApplicationTests {
 	@Test
 	public void contextLoads() {
 		System.out.println("Active profiles: " + Arrays.toString(environment.getActiveProfiles()));
-		// Add more logging as needed
+		System.out.println("Spring DataSource URL: " + environment.getProperty("spring.datasource.url"));
+		System.out.println("Spring DataSource Username: " + environment.getProperty("spring.datasource.username"));
+		// Add additional logging as needed
 	}
 }
